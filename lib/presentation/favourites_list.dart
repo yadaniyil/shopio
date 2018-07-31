dcf1a872979198acede591bc875542309d8f5252
Daniil Yakovlev <danny.yakovlev@gmail.com>
2018-08-01 00:21:15 +0300

    Some favourite item design implementation changes

--- lib/presentation/favourites_list.dart
+++ lib/presentation/favourites_list.dart
@@ -51,34 +51,30 @@ class FavouritesList extends StatelessWidget {
   Widget getRow(BuildContext context, int index) {
     var item = favouriteProducts[index];
     return Card(
-        child: Row(
-//      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Expanded(
-            flex: 6,
-            child: Image.network(
-              '${item.imageLink}',
-              width: 130.0,
-              fit: BoxFit.fitHeight,
-            )),
-        Expanded(
-            flex: 11,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  Text(item.name,
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 18.0)),
-                  tagsSection(item),
-                  priceAndBuySection(item)
-                ],
-              ),
-            ))
-      ],
+        child: Container(
+      height: 130.0,
+      child: Row(
+        children: <Widget>[
+          Image.network(
+            '${item.imageLink}',
+            fit: BoxFit.fitHeight,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0, left: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(item.name,
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0)),
+                tagsSection(item),
+                priceAndBuySection(item)
+              ],
+            ),
+          )
+        ],
+      ),
     ));
   }
 
@@ -87,9 +83,10 @@ class FavouritesList extends StatelessWidget {
     if (product.category != null) tags.add(product.category);
     if (product.area != null) tags.add(product.area);
     return Padding(
-      padding: const EdgeInsets.only(top: 12.0),
+      padding: const EdgeInsets.only(top: 8.0),
       child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          verticalDirection: VerticalDirection.up,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: tags
               .map((title) => ActionChip(label: Text(title), onPressed: () {}))
               .toList()),
@@ -98,13 +95,12 @@ class FavouritesList extends StatelessWidget {
 
   Widget priceAndBuySection(ProductModel product) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: <Widget>[
         Text(product.getPrice(),
             style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0)),
         IconButton(
             icon: Icon(Icons.add_shopping_cart, color: Colors.blueAccent),
-        onPressed: () => {})
+            onPressed: () => {})
       ],
     );
   }
